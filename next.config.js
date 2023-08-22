b93bde4e72eae9642774832a776dcd93158e9f30
Mikaël Ruffieux <mikael@redoxdigital.ch>
2023-08-22 15:49:27 +0200

    npm run export no more needed
    The site is now exported by default.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, trailingSlash: true, env: {
-    api: "https://mydomain.com/api",
-    phone: "+41 00 000 00 00",
-    mail: "info@mydomain.com",
-    ig: "https://www.instagram.com/myaccount/",
-    fb: "https://www.facebook.com/myaccount",
-    in: "https://www.linkedin.com/company/myaccount"
-} };
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    api: 'https://mydomain.com/api',
+    phone: '+41 00 000 00 00',
+    mail: 'info@mydomain.com',
+    ig: 'https://www.instagram.com/myaccount/',
+    fb: 'https://www.facebook.com/myaccount',
+    in: 'https://www.linkedin.com/company/myaccount',
+  },
+};
 
 module.exports = nextConfig;
