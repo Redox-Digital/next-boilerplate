40d8bafb9ee013227e96cded5d6ca5b38f0a5176
Mikaël Ruffieux <mikael@redoxdigital.ch>
2025-11-26 12:39:20 +0100

    404 using components

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,17 +1,13 @@
 import Button from '@/components/navigation/Button';
-import css from './404.module.scss';
+import { TextHero } from '@/components/layouts/Hero';
 
 export default function NotFoundPage() {
   return (
-    <section className={`light ${css.notFoundPage}`}>
-      <div className="container">
-        <h5>Erreur 404</h5>
-        <h2>Page introuvable</h2>
-        <p>La page que vous recherchez n&rsquo;existe pas.</p>
-        <Button ariaLabel="Retourner à la page accueil" to={'/'}>
-          Retour à l&rsquo;accueil
-        </Button>
-      </div>
-    </section>
+    <TextHero title="Page introuvable" surtitle="Erreur 404" half>
+      <>
+        <p>La page que vous recherchez n’existe pas.</p>
+        <Button href="/">Retour à l’accueil</Button>
+      </>
+    </TextHero>
   );
 }
